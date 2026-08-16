0cce916ee9ccac66785fddbef6138710e5f2b123
prespolis1400 <128758467+prespolis1400@users.noreply.github.com>
2026-08-17 00:04:15 +0330

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+// sw.js
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
